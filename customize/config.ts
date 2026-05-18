c4132f42d77eab4c7844c19a162aa5cf850fd4b3
amq-tutti <tutti-amq@proton.me>
2026-05-18 20:41:16 +0300

    Replace previous test batch of songs with new ones + disable google sheets in `main` branch

--- customize/config.ts
+++ customize/config.ts
@@ -2,12 +2,13 @@ import type { AppConfig } from "../src/app/types";
 
 export const config = {
   localStoragePrefix: "test-sorter",
-  title: "Test! Sorter",
+  title: "Test Sorter",
   description: "Party rank sorter for your custom list of songs.",
-  googleSheets: {
-    clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
-    appId: "601853881036",
-    rankColumnHeader: "Rank",
-    scoreColumnHeader: "Score (optional)",
-  },
+  // Uncomment the following lines to enable Google Sheets integration. Make sure to fill in the correct values.
+  // googleSheets: {
+  //   clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
+  //   appId: "601853881036",
+  //   rankColumnHeader: "Rank",
+  //   scoreColumnHeader: "Score (optional)",
+  // },
 } satisfies AppConfig;
