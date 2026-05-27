e111a8a2ae08b7536387069863784d88f40f4da4
Jerry Lee <77219284+darkcb109@users.noreply.github.com>
2026-05-26 21:47:05 -0400

    test

--- customize/config.ts
+++ customize/config.ts
@@ -5,13 +5,13 @@ export const config = {
   title: "Test Sorter",
   description: "Party rank sorter for your custom list of songs.",
   // Uncomment the following lines to enable Google Sheets integration. Make sure to fill in the correct values.
-  // googleSheets: {
-  //   clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
-  //   appId: "601853881036",
-  //   rankColumnHeader: "Rank",
-  //   This setting will only work if `scoreColumnHeader` is not set, allowing users to specify their own score column header in the sheet.
-  //   If `scoreColumnHeader` is set, that value will be used as the score column header and users won't be able to change it.
-  //   allowCustomScoreColumn: true,
-  //   scoreColumnHeader: "Score (optional)",
-  // },
+   googleSheets: {
+     clientId: "2627545528-0frh9psfhjpc57o6ru9h5o0lbeqdrk1u.apps.googleusercontent.com",
+     appId: " pr-project-497601",
+     rankColumnHeader: "Rank",
+     This setting will only work if `scoreColumnHeader` is not set, allowing users to specify their own score column header in the sheet.
+     If `scoreColumnHeader` is set, that value will be used as the score column header and users won't be able to change it.
+     allowCustomScoreColumn: true,
+     scoreColumnHeader: "Score (optional)",
+   },
 } satisfies AppConfig;
